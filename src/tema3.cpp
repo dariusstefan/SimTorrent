a7b3e75e67a9c82e71463e06606c4a1693a898fe
dariusstefan <darius.stefan.02@gmail.com>
2024-01-14 19:47:20 +0200

    Gata

--- src/tema3.cpp
+++ src/tema3.cpp
@@ -1,20 +1,191 @@
 #include "tema3.hpp"
 
-#define TRACKER_RANK 0
-#define MAX_FILES 10
-#define MAX_FILENAME 15
-#define HASH_SIZE 32
-#define MAX_CHUNKS 100
-
 void download_thread_func(const ThreadArgs &args) {
+    int rank = args.rank;
+    unordered_map<string, int> *owned_files = args.owned_files;
+    unordered_map<string, vector<string>> *owned_chunks = args.owned_chunks;
+    unordered_map<string, int> *wanted_files = args.wanted_files;
+    unordered_map<string, vector<string>> *wanted_chunks = args.wanted_chunks;
+    unordered_map<string, set<int>> *chunk_peers = args.chunk_peers;
+
+    int max_chunks = 10;
+
+    for (auto &file : *wanted_files) {
+        char filename[MAX_FILENAME];
+        strcpy(filename, file.first.c_str());
+        bool complete = false;
+        int current_chunk = 0;
+        while (!complete) {
+            if (file.second == -1) {
+                int req = 0;
+                MPI_Send(&req, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+                MPI_Send(filename, MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
+                MPI_Recv(&file.second, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                for (int i = 0; i < file.second; ++i) {
+                    char chunk_hash[HASH_SIZE + 1];
+                    MPI_Recv(chunk_hash, HASH_SIZE + 1, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    (*wanted_chunks)[file.first].push_back(string(chunk_hash));
+                }
+            } else {
+                int req = 1;
+                MPI_Send(&req, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+                MPI_Send(filename, MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
+
+                int num_peers;
+                MPI_Recv(&num_peers, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                for (int i = 0; i < num_peers; ++i) {
+                    int peer_rank;
+                    MPI_Recv(&peer_rank, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                    int num_chunks;
+                    MPI_Recv(&num_chunks, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                    for (int j = 0; j < num_chunks; ++j) {
+                        char chunk_hash[HASH_SIZE + 1];
+                        MPI_Recv(chunk_hash, HASH_SIZE + 1, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                        (*chunk_peers)[string(chunk_hash)].insert(peer_rank);
+                    }
+                }
+
+                while (current_chunk < file.second && max_chunks) {
+                    string chunk_hash = (*wanted_chunks)[file.first][current_chunk];
+                    int num_peers = (*chunk_peers)[chunk_hash].size();
+
+                    int chosen_peer_idx = rand() % num_peers;
+                    set<int>::iterator it = (*chunk_peers)[chunk_hash].begin();
+                    advance(it, chosen_peer_idx);
+
+                    int peer_rank = *it;
+
+                    char hash[HASH_SIZE + 1];
+                    strcpy(hash, chunk_hash.c_str());
+                    MPI_Send(hash, HASH_SIZE + 1, MPI_CHAR, peer_rank, 1, MPI_COMM_WORLD);
+                    
+                    int ok;
+                    MPI_Recv(&ok, 1, MPI_INT, peer_rank, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                    (*owned_files)[file.first] = current_chunk + 1;
+                    (*owned_chunks)[file.first].push_back(chunk_hash);
+
+                    current_chunk++;
+                    max_chunks--;
+                }
+
+                if (current_chunk == file.second) {
+                    complete = true;
+                    ofstream fout("client" + to_string(rank) + "_" + file.first);
+
+                    for (auto chunk : (*owned_chunks)[file.first]) {
+                        fout << chunk << '\n';
+                    }
+
+                    fout.close();
+                } else {
+                    max_chunks = 10;
+
+                    int req = 3;
+                    MPI_Send(&req, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+                    
+                    send_refresh_tracker(args);
+                }
+            }
+        }
+    }
+
+    int req = 2;
+    MPI_Send(&req, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
 }
 
 void upload_thread_func(const ThreadArgs &args) {
+    while (true) {
+        MPI_Status status;
+
+        char chunk_hash[HASH_SIZE + 1];
+        MPI_Recv(chunk_hash, HASH_SIZE + 1, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+        int peer_rank = status.MPI_SOURCE;
+
+        string chunk_hash_str(chunk_hash);
+
+        if (chunk_hash_str == "!stop!") {
+            break;
+        }
+
+        MPI_Send(&peer_rank, 1, MPI_INT, peer_rank, 2, MPI_COMM_WORLD);
+    }
 }
 
 void tracker(int numtasks, int rank) {
-    unordered_map<string, vector<string>> files;
-    unordered_map<string, unordered_map<string, vector<int>>> swarm;
+    Tracker tracker_service;
+
+    for (int i = 1; i < numtasks; ++i) {
+        tracker_service.recv_refresh(i, true);
+    }
+
+    for (int i = 1; i < numtasks; ++i) {
+        int ack = 1;
+        MPI_Send(&ack, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+    }
+
+    int connected_peers = numtasks - 1;
+    while (connected_peers) {
+        int req;
+        MPI_Status status;
+        MPI_Recv(&req, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+        int peer_rank = status.MPI_SOURCE;
+
+        if (req == 0) {
+            char filename[MAX_FILENAME];
+            MPI_Recv(filename, MAX_FILENAME, MPI_CHAR, peer_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            string filename_str(filename);
+
+            int num_chunks = tracker_service.files[filename_str].size();
+            MPI_Send(&num_chunks, 1, MPI_INT, peer_rank, 0, MPI_COMM_WORLD);
+
+            for (auto chunk : tracker_service.files[filename_str]) {
+                char chunk_hash[HASH_SIZE + 1];
+                strcpy(chunk_hash, chunk.c_str());
+                MPI_Send(chunk_hash, HASH_SIZE + 1, MPI_CHAR, peer_rank, 0, MPI_COMM_WORLD);
+            }
+        }
+        
+        if (req == 1) {
+            char filename[MAX_FILENAME];
+            MPI_Recv(filename, MAX_FILENAME, MPI_CHAR, peer_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            string filename_str(filename);
+
+            int num_peers = tracker_service.swarm[filename_str].size();
+            MPI_Send(&num_peers, 1, MPI_INT, peer_rank, 0, MPI_COMM_WORLD);
+
+            for (auto peer : tracker_service.swarm[filename_str]) {
+                MPI_Send(&peer.first, 1, MPI_INT, peer_rank, 0, MPI_COMM_WORLD);
+
+                int num_chunks = peer.second.size();
+                MPI_Send(&num_chunks, 1, MPI_INT, peer_rank, 0, MPI_COMM_WORLD);
+
+                for (auto chunk : peer.second) {
+                    char chunk_hash[HASH_SIZE + 1];
+                    strcpy(chunk_hash, chunk.c_str());
+                    MPI_Send(chunk_hash, HASH_SIZE + 1, MPI_CHAR, peer_rank, 0, MPI_COMM_WORLD);
+                }
+            }
+        }
+
+        if (req == 2) {
+            connected_peers--;
+        }
+
+        if (req == 3) {
+            tracker_service.recv_refresh(peer_rank, false);
+        }
+    }
+
+    for (int i = 1; i < numtasks; ++i) {
+        char chunk_hash[HASH_SIZE + 1];
+        strcpy(chunk_hash, "!stop!");
+        MPI_Send(chunk_hash, HASH_SIZE + 1, MPI_CHAR, i, 1, MPI_COMM_WORLD);
+    }
 }
 
 void peer(int numtasks, int rank) {
@@ -56,40 +227,6 @@ void peer(int numtasks, int rank) {
 
     fin.close();
 
-    ofstream fout("client" + to_string(rank) + ".txt");
-    fout << "Peer " << rank << ":\n";
-    fout << "Owned files:\n";
-
-    for (auto &file : owned_files) {
-        fout << file.first << " " << file.second << "\n";
-    }
-
-    fout << "Owned chunks:\n";
-
-    for (auto &file : owned_chunks) {
-        fout << file.first << "\n";
-
-        for (auto &chunk : file.second) {
-            fout << chunk << "\n";
-        }
-
-        fout << "\n";
-    }
-
-    fout << "Wanted files:\n";
-
-    for (auto &file : wanted_files) {
-        fout << file.first << "\n";
-
-        for (auto &chunk : wanted_chunks[file.first]) {
-            fout << chunk << "\n";
-        }
-
-        fout << "\n";
-    }
-
-    fout.close();
-
     unordered_map<string, set<int>> chunk_peers;
 
     for (auto &file : wanted_files) {
@@ -105,6 +242,11 @@ void peer(int numtasks, int rank) {
     args.wanted_files = &wanted_files;
     args.wanted_chunks = &wanted_chunks;
     args.chunk_peers = &chunk_peers;
+
+    send_refresh_tracker(args);
+
+    int ack;
+    MPI_Recv(&ack, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     thread download_thread(download_thread_func, args);
     thread upload_thread(upload_thread_func, args);
--- src/tema3.hpp
+++ src/tema3.hpp
@@ -7,6 +7,12 @@
 #include <set>
 #include <vector>
 
+#define TRACKER_RANK 0
+#define MAX_FILES 10
+#define MAX_FILENAME 15
+#define HASH_SIZE 32
+#define MAX_CHUNKS 100
+
 using namespace std;
 
 struct ThreadArgs {
@@ -16,4 +22,61 @@ struct ThreadArgs {
     unordered_map<string, int> *wanted_files;
     unordered_map<string, vector<string>> *wanted_chunks;
     unordered_map<string, set<int>> *chunk_peers;
+};
+
+void send_refresh_tracker(const ThreadArgs &args) {
+    int num_files = args.owned_files->size();
+    MPI_Send(&num_files, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+    
+    for (auto file : *args.owned_files) {
+        char filename[MAX_FILENAME];
+        strcpy(filename, file.first.c_str());
+        MPI_Send(filename, MAX_FILENAME, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
+        MPI_Send(&file.second, 1, MPI_INT, TRACKER_RANK, 0, MPI_COMM_WORLD);
+        for (auto chunk : (*args.owned_chunks)[file.first]) {
+            char chunk_hash[HASH_SIZE + 1];
+            strcpy(chunk_hash, chunk.c_str());
+            MPI_Send(chunk_hash, HASH_SIZE + 1, MPI_CHAR, TRACKER_RANK, 0, MPI_COMM_WORLD);
+        }
+    }
+}
+
+struct Tracker {
+    unordered_map<string, vector<string>> files;
+    unordered_map<string, unordered_map<int, vector<string>>> swarm;
+
+    void recv_refresh(int peer, bool init) {
+        int num_files;
+        MPI_Recv(&num_files, 1, MPI_INT, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        for (int j = 0; j < num_files; j++) {
+            char filename[MAX_FILENAME];
+            MPI_Recv(filename, MAX_FILENAME, MPI_CHAR, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            string filename_str(filename);
+
+            int num_chunks;
+            MPI_Recv(&num_chunks, 1, MPI_INT, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            for (int k = 0; k < num_chunks; ++k) {
+                char chunk_hash[HASH_SIZE + 1];
+                MPI_Recv(chunk_hash, HASH_SIZE + 1, MPI_CHAR, peer, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                string chunk_hash_str(chunk_hash);
+
+                swarm[filename_str][peer].push_back(chunk_hash_str);
+
+                if (!init)
+                    continue;
+                
+                if (files.find(filename_str) != files.end()) {
+                    if (files[filename_str].size() < num_chunks) {
+                        files[filename_str].push_back(chunk_hash_str);
+                    }
+                } else {
+                    files[filename_str].push_back(chunk_hash_str);
+                }
+            }
+        }
+    }
 };
